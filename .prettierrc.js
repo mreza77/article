c6b11ae5bd9adda488ff6811faa3db7eaae05a26
mohammad reza <mojaver77@gmail.com>
2022-02-15 01:03:34 +0330

    Feat: creat navigation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-};
+{
+  "trailingComma": "es5",
+    "tabWidth": 3,
+      "semi": true,
+        "singleQuote": false
+}
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, Platform, View, Text } from 'react-native';
+
+
+
+const HomeScreen = () => {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Home Screen</Text>
+      </View>
+   )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+   screen: {
+
+   }
+});
